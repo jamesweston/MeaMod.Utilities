693d7821bebf4e541b26c06fb38487764e049a0b
James Weston <james@fe0.uk>
2021-06-24 22:45:23 +0100

    Added WinForms UI Library

---
+++ MeaMod.Utilities.UI.WinForms/ServiceStatusLabel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MeaMod.Utilities.UI.WinForms
+{
+    public class ServiceStatusLabel : Control
+    {
+        private string _Status = "Demo";
+        private Color _StatusColour = Color.Green;
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [Description("Service Status")]
+        [Browsable(true)]
+        public string Status
+        {
+            get
+            {
+                return _Status;
+            }
+
+            set
+            {
+                _Status = value;
+                Invalidate();
+            }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [Description("Status Color")]
+        [Browsable(true)]
+        public Color StatusColour
+        {
+            get
+            {
+                return _StatusColour;
+            }
+
+            set
+            {
+                _StatusColour = value;
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var textMeasure = TextRenderer.MeasureText(e.Graphics, "Service Status: ", Font);
+            var statusMeasure = TextRenderer.MeasureText(e.Graphics, _Status, Font);
+            TextRenderer.DrawText(e.Graphics, "Service Status: ", Font, new Point(2, (int)Math.Round(ClientSize.Height / 2d - textMeasure.Height / 2d)), ForeColor);
+            TextRenderer.DrawText(e.Graphics, _Status, Font, new Point(textMeasure.Width, (int)Math.Round(ClientSize.Height / 2d - statusMeasure.Height / 2d)), _StatusColour);
+        }
+    }
+}

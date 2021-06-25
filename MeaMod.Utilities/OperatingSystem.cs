6c6078392b1b58d866a050bc383b84c57f1be413
James Weston <james@fe0.uk>
2021-06-25 10:12:29 +0100

    Added OperatingSystem class

---
+++ MeaMod.Utilities/OperatingSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MeaMod.Utilities
+{
+    public static class OperatingSystem
+    {
+        public static bool IsWindows() =>
+#if NETFRAMEWORK
+           true;
+#else
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+#endif
+
+        public static bool IsMacOS() =>
+#if NETFRAMEWORK
+           false;
+#else
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+#endif
+
+        public static bool IsLinux() =>
+#if NETFRAMEWORK
+           false;
+#else
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+#endif
+
+    }
+}

6e662228f48c1aa5f8fc5db902ee9a7c65b45c4b
James Weston <james@fe0.uk>
2022-08-19 15:45:17 +0100

    Added Zip Extract with Progress class

---
+++ MeaMod.Utilities/Zip/ZipProgress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeaMod.Utilities.Zip
+{
+    public class ZipProgress
+    {
+        public ZipProgress(int total, int processed, string currentItem)
+        {
+            Total = total;
+            Processed = processed;
+            CurrentItem = currentItem;
+        }
+        public int Total { get; }
+        public int Processed { get; }
+        public string CurrentItem { get; }
+    }
+}

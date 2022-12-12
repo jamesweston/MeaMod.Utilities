4a8bec46ca246187767f09fe2c0eb5546cd964d0
James Weston <james@fe0.uk>
2022-12-12 18:16:19 +0000

    Added RandomExtensions and X509AuthorityKeyIdentifierExtension

---
+++ MeaMod.Utilities/Cryptography/X509AuthorityKeyIdentifierExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Diagnostics.Contracts;
+
+namespace MeaMod.Utilities.Cryptography
+{
+    /// <summary>
+    /// Add X509AuthorityKeyIdentifierExtension to .net standard 2.0
+    /// </summary>
+    public class X509AuthorityKeyIdentifierExtension : X509Extension
+    {
+        private static Oid AuthorityKeyIdentifierOid => new Oid("2.5.29.35");
+        private static Oid SubjectKeyIdentifierOid => new Oid("2.5.29.14");
+
+        /// <summary>
+        /// Initializes a new instance of the X509AuthorityKeyIdentifierExtension class using a X509Certificate2 and a value indicating whether the extension is critical.
+        /// </summary>
+        /// <param name="certificateAuthority">X509Certificate2</param>
+        /// <param name="critical">Boolean</param>
+        public X509AuthorityKeyIdentifierExtension(X509Certificate2 certificateAuthority, bool critical)
+            : base(AuthorityKeyIdentifierOid, EncodeExtension(certificateAuthority), critical)
+        {
+        }
+
+        private static byte[] EncodeExtension(X509Certificate2 certificateAuthority)
+        {
+            var subjectKeyIdentifier = certificateAuthority.Extensions.Cast<X509Extension>().FirstOrDefault(p => p.Oid?.Value == SubjectKeyIdentifierOid.Value);
+            if (subjectKeyIdentifier == null)
+                return null;
+            var rawData = subjectKeyIdentifier.RawData;
+            var segment = new ArraySegment<byte>(rawData, 2, rawData.Length - 2);
+            var authorityKeyIdentifier = new byte[segment.Count + 4];
+            // KeyID of the AuthorityKeyIdentifier
+            authorityKeyIdentifier[0] = 0x30;
+            authorityKeyIdentifier[1] = 0x16;
+            authorityKeyIdentifier[2] = 0x80;
+            authorityKeyIdentifier[3] = 0x14;
+            //segment.CopyTo(authorityKeyIdentifier, 4);
+            Array.Copy(segment.Array,0,authorityKeyIdentifier,4,segment.Count);
+            return authorityKeyIdentifier;
+        }
+    }
+}
---
+++ MeaMod.Utilities/RandomExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MeaMod.Utilities
+{
+    /// <summary>
+    /// Extensions to Random Class
+    /// </summary>
+    public class RandomExtensions : Random
+    {
+        /// <summary>
+        /// Create Random BigInteger with range
+        /// </summary>
+        /// <param name="min">Lower Limit</param>
+        /// <param name="max">Upper Limit</param>
+        /// <returns></returns>
+        public BigInteger CreateRandomInRange(BigInteger min, BigInteger max)
+        {
+            string numeratorString, denominatorString;
+            double fraction = this.NextDouble();
+            BigInteger inRange;
+
+            //Maintain all 17 digits of precision, 
+            //but remove the leading zero and the decimal point;
+            numeratorString = fraction.ToString("G17").Remove(0, 2);
+
+            //Use the length instead of 17 in case the random
+            //fraction ends with one or more zeros
+            denominatorString = string.Format("1E{0}", numeratorString.Length);
+
+            inRange = (max - min) * BigInteger.Parse(numeratorString) /
+               BigInteger.Parse(denominatorString,
+               System.Globalization.NumberStyles.AllowExponent)
+               + min;
+            return inRange;
+        }
+    }
+}
